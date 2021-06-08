094c87620e604901a6e624991d3d85dc821942ae
Vamsi <vramisetti@aligntech.com>
2021-06-08 16:13:02 +0530

    added echo statements

--- lib/pwa_app_version/init.rb
+++ lib/pwa_app_version/init.rb
@@ -7,8 +7,10 @@ module PwaAppVersion
   # end
   # copy_with_path(src, dst)
   RAILS_APP_PATH = ENV['PWD']
+  `echo '------coping files---------'`
   `cp pre-push #{RAILS_APP_PATH}/.git/hooks`
   `cp .pre_push.rb #{RAILS_APP_PATH}/.git/hooks`
+  `echo '------changing permissions files---------'`
   `chmod 755  #{RAILS_APP_PATH}/.git/hooks/post-push`
   `chmod 755  #{RAILS_APP_PATH}/.git/hooks/.post_push.rb`
 end

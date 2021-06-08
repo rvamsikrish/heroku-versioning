4c2e6e360e0ebf647e39ecf2a97ceac616dad1f4
Vamsi <vramisetti@aligntech.com>
2021-06-08 21:43:22 +0530

    changes

--- .pre_push.rb
+++ .pre_push.rb
@@ -16,7 +16,7 @@ class PrePush
     version_num = `git rev-list HEAD --count`
     commit_hash = `git rev-parse Head`
     date_val = `git show -s --format=%ci #{commit_hash}`
-    "#{date_val.split(' ')[0]}.#{version_num}"
+    "#{date_val.split(' ')[0].split("-").join(".")}_#{version_num}"
   end
 end
 

baa4453a2926b8512d044d48c92fc1ba8003670b
Vamsi <vramisetti@aligntech.com>
2021-06-08 19:36:15 +0530

    changes

--- lib/pwa_app_version/init.rb
+++ lib/pwa_app_version/init.rb
@@ -8,9 +8,9 @@ module PwaAppVersion
   # copy_with_path(src, dst)
   RAILS_APP_PATH = ENV['PWD']
   `echo '------coping files---------'`
-  `cp post-push #{RAILS_APP_PATH}/.git/hooks`
-  `cp .post_push.rb #{RAILS_APP_PATH}/.git/hooks`
+  `cp pre-push #{RAILS_APP_PATH}/.git/hooks`
+  `cp .pre_push.rb #{RAILS_APP_PATH}/.git/hooks`
   `echo '------coping files---------'`
-  `chmod 755  #{RAILS_APP_PATH}/.git/hooks/post-push`
-  `chmod 755  #{RAILS_APP_PATH}/.git/hooks/.post_push.rb`
+  `chmod 755  #{RAILS_APP_PATH}/.git/hooks/pre-push`
+  `chmod 755  #{RAILS_APP_PATH}/.git/hooks/.pre_push.rb`
 end
